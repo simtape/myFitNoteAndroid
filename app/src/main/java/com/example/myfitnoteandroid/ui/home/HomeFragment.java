9634f5f4bd9fb7353f767de2ca609b0ba4cdfad9
simtape <s.cassetta1@studenti.unimol.it>
2021-02-05 00:38:46 +0100

    ho fatto i campi login animati e ora il menu è accessibile da tutti i fragment

--- app/src/main/java/com/example/myfitnoteandroid/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/myfitnoteandroid/ui/home/HomeFragment.java
@@ -16,20 +16,15 @@ import com.example.myfitnoteandroid.R;
 
 public class HomeFragment extends Fragment {
 
-    private HomeViewModel homeViewModel;
+
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        homeViewModel =
-                ViewModelProviders.of(this).get(HomeViewModel.class);
-        View root = inflater.inflate(R.layout.fragment_home, container, false);
-        final TextView textView = root.findViewById(R.id.text_home);
-        homeViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
-            @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
-            }
-        });
+
+        ViewGroup root = (ViewGroup) inflater.inflate(R.layout.fragment_home, container, false);
+        TextView textView = root.findViewById(R.id.text_home);
+        textView.setText("provo a vedere se va senza model");
+
         return root;
     }
 }
