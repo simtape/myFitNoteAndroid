6c23d1313a29c04b2ceefd6faa013d4f78e00510
simtape <s.cassetta1@studenti.unimol.it>
2021-02-12 18:50:40 +0100

    update obiettivi 2

--- app/src/main/java/com/example/myfitnoteandroid/ui/goals/GoalsAdapter.java
+++ app/src/main/java/com/example/myfitnoteandroid/ui/goals/GoalsAdapter.java
@@ -1,7 +1,6 @@
 package com.example.myfitnoteandroid.ui.goals;
 
 import android.content.Context;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -22,7 +21,7 @@ public class GoalsAdapter extends ArrayAdapter<String> {
     int rImgs[];
     long item;
     GoalsAdapter(Context c, String[] title, String[] description, int imgs[]) {
-        super(c, R.layout.goals_row, R.id.goalstv1, title);
+        super(c, R.layout.goals_row, R.id.tvWater, title);
         this.context = c;
         this.rTitle = title;
         this.rDesciption = description;
@@ -34,8 +33,8 @@ public class GoalsAdapter extends ArrayAdapter<String> {
     public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
         LayoutInflater layoutInflater = (LayoutInflater) context.getApplicationContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
         View goals_row = layoutInflater.inflate(R.layout.goals_row, parent, false);
-        ImageView images = goals_row.findViewById(R.id.image);
-        TextView myTitle = goals_row.findViewById(R.id.goalstv1);
+        ImageView images = goals_row.findViewById(R.id.imageWater);
+        TextView myTitle = goals_row.findViewById(R.id.tvWater);
         TextView myDescription = goals_row.findViewById(R.id.goalstv2);
 
         images.setImageResource(rImgs[position]);
