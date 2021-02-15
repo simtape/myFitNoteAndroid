24fc1650fff60bbc758f0c7d6e09f72b8aabad33
lucacodi <lucacodipietro9@gmail.com>
2021-02-15 17:56:47 +0100

    Obiettivi quasi definitivo manca il bottone grigio

---
+++ app/src/main/java/com/example/myfitnoteandroid/ui/goals/Goal.java
@@ -0,0 +1,39 @@
+package com.example.myfitnoteandroid.ui.goals;
+
+
+public class Goal{
+
+    boolean status_goal;
+    int value_goal;
+    int progress_goal;
+
+
+
+    public void setGoal(boolean status_in,int value_in,int progress_in){
+        setStatus_goal(status_in);
+        setValue_goal(value_in);
+        setProgress_goal(progress_in);
+    }
+
+
+    public void setStatus_goal(boolean status_in) {
+        status_goal = status_in;
+    }
+    public void setValue_goal(int value_in){
+        value_goal = value_in;
+    }
+    public void setProgress_goal(int progress_in){
+        progress_goal = progress_in;
+    }
+
+    public boolean getStatus_goal(){
+        return status_goal;
+    }
+    public int getValue_goal(){
+        return value_goal;
+    }
+    public int getProgress_goal(){
+        return progress_goal;
+    }
+
+}
--- app/src/main/java/com/example/myfitnoteandroid/ui/goals/GoalsFragment.java
+++ app/src/main/java/com/example/myfitnoteandroid/ui/goals/GoalsFragment.java
@@ -1,8 +1,5 @@
 package com.example.myfitnoteandroid.ui.goals;
 
-import androidx.annotation.RequiresApi;
-
-import android.os.Build;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -10,14 +7,16 @@ import androidx.annotation.Nullable;
 import androidx.cardview.widget.CardView;
 import androidx.fragment.app.Fragment;
 
+import android.text.Editable;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Switch;
-import android.widget.TextClock;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
@@ -33,23 +32,24 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.util.Arrays;
+
 
 public class GoalsFragment extends Fragment {
 
     String mTitle[] = {"Acqua", "Grassi Bruciati"};
-    String mDescription[] = {"Quantità di acqua da bere", "Calorie da bruciare"};
     Button conferma;
-    Boolean userGoals[] = {false, false};
-    int progressGoals[];
-    int valueGoals[];
-    int images[] = {R.drawable.ic_baseline_local_drink_24, R.drawable.ic_kcal};
+    String id_user;
+    Goal water_goal = new Goal();
+    Goal kcal_goal = new Goal();
     Switch waterSwitch, kcalSwitch;
     ViewGroup root;
     private JSONObject postData;
-    private JSONObject goals;
-    private JSONObject newGoal;
+    JSONObject local_goal;
     int arraySize;
     TextView water, kcal;
+    EditText kcal_value,water_value;
+    JSONObject up;
     public static GoalsFragment newInstance() {
         return new GoalsFragment();
     }
@@ -59,10 +59,11 @@ public class GoalsFragment extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
         getGoals();
-
         root = (ViewGroup) inflater.inflate(R.layout.goals_fragment, container, false);
         water = root.findViewById(R.id.tvWater);
         kcal = root.findViewById(R.id.tvKcal);
+        kcal_value = root.findViewById(R.id.kcal_value_num);
+        water_value = root.findViewById(R.id.water_value_num);
         waterSwitch = root.findViewById(R.id.switchWater);
         kcalSwitch = root.findViewById(R.id.switchKcal);
         water.setText(mTitle[0]);
@@ -79,10 +80,10 @@ public class GoalsFragment extends Fragment {
         SessionManager sessionManager = new SessionManager(getContext());
         try {
             postData.put("id_user", sessionManager.getSession());
-            Log.d("user_id", sessionManager.getSession());
+            id_user = sessionManager.getSession();
+            Log.d("user_id", id_user);
         } catch (JSONException e) {
             e.printStackTrace();
-
         }
 
         String url = "https://myfitnote.herokuapp.com/goals/get_goals";
@@ -92,21 +93,18 @@ public class GoalsFragment extends Fragment {
             public void onResponse(JSONObject response) {
                 try {
                     JSONObject res = response.getJSONObject("goal");
-                    goals = response.getJSONObject("goal");
-                    Log.d("status to string", goals.toString());
                     if (res != null) {
                         JSONArray status = res.getJSONArray("status");
-                        Log.d("status to string", status.toString());
-                        userGoals = new Boolean[status.length()];
-                        for (int i = 0; i < status.length(); i++) {
-                            userGoals[i] = status.getBoolean(i);
-                        }
+                        JSONArray value = res.getJSONArray("obiettivo");
+                        JSONArray progress = res.getJSONArray("valore_attuale");
+                        water_goal.setGoal(status.getBoolean(0),value.getInt(0),progress.getInt(0));
+                        kcal_goal.setGoal(status.getBoolean(1),value.getInt(1),progress.getInt(1));
+                        local_goal = res;
                         setSwitches();
                     }
-
                 } catch (JSONException e) {
                     e.printStackTrace();
-
+                    createGoals();
                 }
 
 
@@ -139,12 +137,18 @@ public class GoalsFragment extends Fragment {
     }
 
     public void createGoals() {
-        postData = new JSONObject();
-        SessionManager sessionManager = new SessionManager(getContext());
+        Boolean[] status_array = new Boolean[]{false,false};
+        JSONArray status_jarray = new JSONArray(Arrays.asList(status_array));
+        Integer[] value_array = new Integer[]{3,4000};
+        JSONArray value_jarray = new JSONArray(Arrays.asList(value_array));
+        Integer[] prog_array = new Integer[]{0,0};
+        JSONArray prog_jarray = new JSONArray(Arrays.asList(prog_array));
         try {
-            postData.put("id_user", sessionManager.getSession());
-            Log.d("user_id", sessionManager.getSession());
-            Log.d("user_id", "d" + postData.getBoolean("status[0]"));
+            postData.put("status",status_jarray);
+            postData.put("obiettivo",value_jarray);
+            postData.put("valore_attuale",prog_jarray);
+            postData.put("id_user", id_user);
+
         } catch (JSONException e) {
             e.printStackTrace();
         }
@@ -154,14 +158,90 @@ public class GoalsFragment extends Fragment {
             @Override
             public void onResponse(JSONObject response) {
                 try {
-
-
                     JSONObject res = response.getJSONObject("goal");
-                    if (res != null) {
-                        JSONArray status = res.getJSONArray("status");
-                        Log.d("status to string", status.toString());
-                    }
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
 
+
+            }
+        }, new Response.ErrorListener() {
+            public void onErrorResponse(VolleyError error) {
+                error.printStackTrace();
+            }
+
+        });
+        jsonObjectRequest.setRetryPolicy(new RetryPolicy() {
+            @Override
+            public int getCurrentTimeout() {
+                return 50000;
+            }
+
+            @Override
+            public int getCurrentRetryCount() {
+                return 50000;
+            }
+
+            @Override
+            public void retry(VolleyError error) throws VolleyError {
+
+            }
+        });
+
+        queue.add(jsonObjectRequest);
+        getGoals();
+    }
+
+
+    public void setSwitches() {
+        //ricerca di acqua
+        for (int i = 0; i < mTitle.length; i++) {
+            Log.d("for", "entra");
+
+            if (water_goal.getStatus_goal()) {
+                waterSwitch.setChecked(true);
+            }
+
+        }
+        //ricerca di kcal
+        for (int j = 0; j < mTitle.length; j++) {
+            Log.d("for", "entra");
+
+            if (kcal_goal.getStatus_goal()) {
+                kcalSwitch.setChecked(true);
+            }
+        }
+        setWaterView();
+        setKcalView();
+    }
+
+    public void updateGoals() {
+        postData = new JSONObject();
+        Boolean[] status_array = new Boolean[]{water_goal.getStatus_goal(),kcal_goal.getStatus_goal()};
+        JSONArray status_jarray = new JSONArray(Arrays.asList(status_array));
+        Integer[] value_array = new Integer[]{water_goal.getValue_goal(),kcal_goal.getValue_goal()};
+        JSONArray value_jarray = new JSONArray(Arrays.asList(value_array));
+        Integer[] prog_array = new Integer[]{water_goal.getProgress_goal(),kcal_goal.getProgress_goal()};
+        JSONArray prog_jarray = new JSONArray(Arrays.asList(prog_array));
+        try {
+            postData.put("status",status_jarray);
+            postData.put("obiettivo",value_jarray);
+            postData.put("valore_attuale",prog_jarray);
+            postData.put("id_user", id_user);
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        String url = "https://myfitnote.herokuapp.com/goals/update_goals";
+        Log.d("PostDAta",postData.toString());
+        RequestQueue queue = Volley.newRequestQueue(getActivity().getApplicationContext());
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.POST, url, postData, new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+                try {
+                    JSONObject res = response.getJSONObject("goal");
+                    Log.d("Post",res.toString());
+                    Toast.makeText(getContext(), "Modifica effettuata", Toast.LENGTH_LONG).show();
                 } catch (JSONException e) {
                     e.printStackTrace();
 
@@ -175,61 +255,50 @@ public class GoalsFragment extends Fragment {
             }
 
         });
-
-    }
-
-
-    public void setSwitches() {
-        //ricerca di acqua
-        for (int i = 0; i < mTitle.length; i++) {
-            Log.d("for", "entra");
-
-            if (userGoals[0]) {
-                waterSwitch.setChecked(true);
-                /*value.setVisibility(View.VISIBLE);
-            }else {
-                value.setVisibility(View.INVISIBLE);*/
+        jsonObjectRequest.setRetryPolicy(new RetryPolicy() {
+            @Override
+            public int getCurrentTimeout() {
+                return 50000;
             }
 
-        }
-        //ricerca di kcal
-        for (int j = 0; j < mTitle.length; j++) {
-            Log.d("for", "entra");
+            @Override
+            public int getCurrentRetryCount() {
+                return 50000;
+            }
 
-            if (userGoals[1]) {
-                kcalSwitch.setChecked(true);
-                //value.setVisibility(View.INVISIBLE);
-            }//else {
-                //value.setVisibility(View.INVISIBLE);
-            //}
+            @Override
+            public void retry(VolleyError error) throws VolleyError {
 
-        }
-        setWaterView();
-        setKcalView();
-    }
+            }
+        });
 
-    public void updateGoals() {
-
-
+        queue.add(jsonObjectRequest);
     }
 
     public void confermaButton() {
         conferma = root.findViewById(R.id.conferma);
-        Log.d("msg", "but");
         conferma.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Log.d("msg", "but");
-                if (waterSwitch.isChecked()) {
-                    userGoals[0] = true;
-                } else {
-                    userGoals[0] = false;
-                }
-                if ( kcalSwitch.isChecked()) {
-                    userGoals[1] = true;
-                } else {
-                    userGoals[1] = false;
-                }
+                water_goal.setStatus_goal(waterSwitch.isChecked());
+                kcal_goal.setStatus_goal(kcalSwitch.isChecked());
+                if(water_value.getText().toString().trim().length() != 0){
+                    int w;
+                    w = Integer.parseInt(water_value.getText().toString());
+                if(w>3){
+                    Toast.makeText(getContext(), "MAX 3 Litri", Toast.LENGTH_LONG).show();
+                }else if(w!=0){
+                    water_goal.setValue_goal(w);
+                }}
+                if(kcal_value.getText().toString().trim().length() != 0){
+                    int k;
+                    k = Integer.parseInt(kcal_value.getText().toString());
+                if(k>4000){
+                    Toast.makeText(getContext(), "MAX 4000 Kcal", Toast.LENGTH_LONG).show();
+                }else if(k!=0){
+                    kcal_goal.setValue_goal(k);
+                }}
+                updateGoals();
             }
         });
     }
@@ -255,6 +324,7 @@ public class GoalsFragment extends Fragment {
 
     public void setWaterView() {
         CardView water_value = (CardView) root.findViewById(R.id.water_value);
+        setWaterHint();
         //ricerca di acqua
             if (waterSwitch.isChecked()) {
                 water_value.setVisibility(View.VISIBLE);
@@ -262,13 +332,23 @@ public class GoalsFragment extends Fragment {
                 water_value.setVisibility(View.GONE);
             }
     }
+    public void setWaterHint() {
+        String water = String.valueOf(water_goal.getValue_goal());
+        water_value.setHint(water);
+    }
     public void setKcalView() {
         CardView kcal_value = (CardView) root.findViewById(R.id.kcal_value);
+        setKcalHint();
         if (kcalSwitch.isChecked()) {
             kcal_value.setVisibility(View.VISIBLE);
         }else {
             kcal_value.setVisibility(View.GONE);
         }
     }
+    public void setKcalHint() {
+        String kcal = String.valueOf(kcal_goal.getValue_goal());
+        kcal_value.setHint(kcal);
+    }
+
 
 }
