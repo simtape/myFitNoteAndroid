75ad2ffe526b09771323d6677e272f1db7563b67
simtape <s.cassetta1@studenti.unimol.it>
2021-02-18 00:13:36 +0100

    modifiche home

---
+++ app/src/main/java/com/example/myfitnoteandroid/ui/home/HomeSheetFragment.java
@@ -0,0 +1,34 @@
+package com.example.myfitnoteandroid.ui.home;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.myfitnoteandroid.R;
+
+
+public class HomeSheetFragment extends Fragment {
+
+
+    public HomeSheetFragment() {
+
+    }
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        return inflater.inflate(R.layout.fragment_home_sheet, container, false);
+    }
+}
