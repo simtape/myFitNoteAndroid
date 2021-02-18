267a69750cde00a3b6ac115991e921b8df9279f6
iannonoss <iannonemattia7@gmail.com>
2021-02-18 12:56:57 +0100

    indice massa corporea

--- app/src/main/java/com/example/myfitnoteandroid/ui/bmi/BodyMassFragment.java
+++ app/src/main/java/com/example/myfitnoteandroid/ui/bmi/BodyMassFragment.java
@@ -2,19 +2,39 @@ package com.example.myfitnoteandroid.ui.bmi;
 
 import androidx.lifecycle.ViewModelProvider;
 
+import android.graphics.Color;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.myfitnoteandroid.R;
+import com.example.myfitnoteandroid.data.SessionManager;
 
-public class BodyMassFragment extends Fragment {
+import java.util.ArrayList;
+import java.util.List;
+
+public class BodyMassFragment extends Fragment implements View.OnClickListener {
+
+    Spinner sessoSp;
+    ViewGroup view;
+    TextView sessoTxt, resultIndexTxt;
+    Button indiceC;
+    String yourSesso;
+    final double meter = 0.01;
+    double result;
 
     private BodyMassViewModel mViewModel;
 
@@ -25,14 +45,114 @@ public class BodyMassFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.body_mass_fragment, container, false);
+       view = (ViewGroup) inflater.inflate(R.layout.body_mass_fragment, container, false);
+        sessoTxt = view.findViewById(R.id.textSesso);
+        resultIndexTxt = view.findViewById(R.id.resultIndex);
+        indiceC = view.findViewById(R.id.indice);
+        indiceC.setOnClickListener(this);
+        sessoSp = view.findViewById(R.id.spinnersesso);
+        return view;
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
     }
 
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
-        mViewModel = new ViewModelProvider(this).get(BodyMassViewModel.class);
-        // TODO: Use the ViewModel
+
+        List<String> sesso = new ArrayList<>();
+        sesso.add("- scegli il tuo sesso -");
+        sesso.add("M");
+        sesso.add("F");
+        ArrayAdapter<String> sessoAdapter = new ArrayAdapter<>(getContext(),android.R.layout.simple_spinner_item, sesso );
+        sessoAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        sessoSp.setAdapter(sessoAdapter);
+        sessoSp.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                 yourSesso = sessoSp.getSelectedItem().toString();
+                if(!yourSesso.equals("- scegli il tuo sesso -")){
+                    sessoTxt.setText(yourSesso);
+                }else {
+                    sessoTxt.setText("");
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
     }
 
+    @Override
+    public void onClick(View v) {
+        if (v.getId() == indiceC.getId()) {
+            switch (yourSesso){
+                case "M":
+                    SessionManager sessionManager = new SessionManager(getContext());
+                    String pesoString = sessionManager.getPeso();
+                    int pesoInt = Integer.parseInt(pesoString);
+                    String altezzaStr = sessionManager.getAltezza();
+                    double aletzzaInt = Integer.parseInt(altezzaStr);
+                    aletzzaInt = aletzzaInt*meter;
+                    result = cBMI(pesoInt, aletzzaInt);
+                 if (result<=20.00){
+                     resultIndexTxt.setText("SOTTOPESO");
+                     resultIndexTxt.setBackgroundColor(Color.BLUE);
+                     resultIndexTxt.setTextColor(Color.WHITE);
+                 }else if(20.00<result && result<= 25.00){
+                     resultIndexTxt.setText("NORMOPESO");
+                     resultIndexTxt.setBackgroundColor(Color.GREEN);
+                 }else if(25.00<result && result<= 30.00){
+                     resultIndexTxt.setText("SOVRAPPESO");
+                     resultIndexTxt.setBackgroundColor(Color.YELLOW);
+                 }else if(30.00<result && result<= 40.00){
+                     resultIndexTxt.setText("ADIPOSITA'");
+                     resultIndexTxt.setBackgroundColor(Color.RED);
+                     resultIndexTxt.setTextColor(Color.WHITE);
+                 }else{
+                     Toast.makeText(getContext(), "Non è stato possibile calcolare l'indice", Toast.LENGTH_LONG).show();
+                 }
+                 break;
+
+                case "F":
+                    sessionManager = new SessionManager(getContext());
+                    String pesoStringF = sessionManager.getPeso();
+                    int pesoIntF = Integer.parseInt(pesoStringF);
+                    String altezzaStrF = sessionManager.getAltezza();
+                    double aletzzaIntF = Integer.parseInt(altezzaStrF);
+                    aletzzaIntF = aletzzaIntF*meter;
+                    result = cBMI(pesoIntF, aletzzaIntF);
+                    if (result<=19.00){
+                        resultIndexTxt.setText("SOTTOPESO");
+                        resultIndexTxt.setBackgroundColor(Color.BLUE);
+                        resultIndexTxt.setTextColor(Color.WHITE);
+                    }else if((19.00<result) && (result<= 24.00)){
+                        resultIndexTxt.setText("NORMOPESO");
+                        resultIndexTxt.setBackgroundColor(Color.GREEN);
+                    }else if(24.00<result && result<= 30.00){
+                        resultIndexTxt.setText("SOVRAPPESO");
+                        resultIndexTxt.setBackgroundColor(Color.YELLOW);
+                    }else if(30.00<result && result<= 40.00){
+                        resultIndexTxt.setText("ADIPOSITA'");
+                        resultIndexTxt.setBackgroundColor(Color.RED);
+                        resultIndexTxt.setTextColor(Color.WHITE);
+                    }else{
+                        Toast.makeText(getContext(), "Non è stato possibile calcolare l'indice", Toast.LENGTH_LONG).show();
+                    }
+                break;
+            }
+        }
+    }
+
+    public double cBMI(int peso, double altezza){
+        double cBMi = peso/(altezza*altezza);
+        return cBMi;
+    }
 }
+
